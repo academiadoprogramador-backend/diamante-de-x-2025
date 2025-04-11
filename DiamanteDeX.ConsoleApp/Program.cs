6070b5fbb71d05cfc044222fb41cde078b9e3d4f
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-11 12:43:41 -0300

    Implementado desenho da parte superior do diamante

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -13,6 +13,34 @@
 
             Console.Write("Digite um número ímpar positivo: ");
             int tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
+
+            int quantidadeLinhas = (tamanhoDiamante - 1) / 2;
+
+            int quantidadeDeX = 1;
+            int quantidadeDeEspacos = quantidadeLinhas;
+
+            // Parte superior do diamante
+            for (int linha = 0; linha < quantidadeLinhas; linha++)
+            {
+                // Desenhar os espeços da linha
+                for (int espaco = 0; espaco < quantidadeDeEspacos; espaco++)
+                    Console.Write(" ");
+
+                // Desenhar os x da linha
+                for (int x = 0; x < quantidadeDeX; x++)
+                    Console.Write("x");
+
+                quantidadeDeX += 2;
+                quantidadeDeEspacos--;
+
+                Console.WriteLine();
+            }
+
+            // Parte do meio do diamante
+
+            // Parte inferior do diamante
+
+            Console.ReadLine();
         }
     }
 }
