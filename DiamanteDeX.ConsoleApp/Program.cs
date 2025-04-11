c7da3fcd2fc646631aaabf8b21b595989f297e69
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-11 11:58:36 -0300

    Add project files.

---
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DiamanteDeX.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Diamante de X");
+            Console.WriteLine("---------------------------------------------");
+
+            Console.WriteLine();
+
+            Console.Write("Digite um número ímpar positivo: ");
+            int tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
