72113f77ae9865dcc46d9020a5b7448695775624
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-11 13:28:09 -0300

    Implementada versão estática

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -1,97 +1,33 @@
-﻿namespace DiamanteDeX.ConsoleApp
+﻿namespace DiamanteDeX.ConsoleApp;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static int quantidadeDeLinhas;
-        static int quantidadeDeX;
-        static int quantidadeDeEspacos;
+        ExibirCabecalho();
 
-        static void Main(string[] args)
-        {
-            ExibirCabecalho();
+        int tamanhoDoDiamante = ObterTamanhoDoDiamante();
 
-            int tamanhoDiamante = ObterTamanhoDoDiamante();
+        GeradorDeDiamante.DesenharDiamante(tamanhoDoDiamante);
 
-            quantidadeDeLinhas = (tamanhoDiamante - 1) / 2;
-            quantidadeDeX = 1;
-            quantidadeDeEspacos = quantidadeDeLinhas;
+        Console.ReadLine();
+    }
 
-            DesenharParteSuperior();
+    static void ExibirCabecalho()
+    {
+        Console.Clear();
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine("Diamante de X");
+        Console.WriteLine("---------------------------------------------");
 
-            DesenharParteDoMeio(tamanhoDiamante);
+        Console.WriteLine();
+    }
 
-            DesenharParteInferior();
+    static int ObterTamanhoDoDiamante()
+    {
+        Console.Write("Digite um número ímpar positivo: ");
+        int tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine();
-        }
-
-        static void ExibirCabecalho()
-        {
-            Console.Clear();
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("Diamante de X");
-            Console.WriteLine("---------------------------------------------");
-
-            Console.WriteLine();
-        }
-
-        static int ObterTamanhoDoDiamante()
-        {
-            Console.Write("Digite um número ímpar positivo: ");
-            int tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
-
-            return tamanhoDiamante;
-        }
-
-        static void DesenharParteSuperior()
-        {
-            Console.WriteLine();
-
-            for (int linha = 0; linha < quantidadeDeLinhas; linha++)
-            {
-                // Desenhar os espeços da linha
-                for (int espaco = 0; espaco < quantidadeDeEspacos; espaco++)
-                    Console.Write(" ");
-
-                // Desenhar os x da linha
-                for (int x = 0; x < quantidadeDeX; x++)
-                    Console.Write("x");
-
-                quantidadeDeX += 2;
-                quantidadeDeEspacos--;
-
-                Console.WriteLine();
-            }
-        }
-
-        static void DesenharParteDoMeio(int tamanhoDiamante)
-        {
-            for (int x = 0; x < tamanhoDiamante; x++)
-                Console.Write("x");
-
-            Console.WriteLine();
-        }
-
-        static void DesenharParteInferior()
-        {
-            quantidadeDeX -= 2;
-            quantidadeDeEspacos = 1;
-
-            for (int linha = 0; linha < quantidadeDeLinhas; linha++)
-            {
-                // Desenhar os espeços da linha
-                for (int espaco = 0; espaco < quantidadeDeEspacos; espaco++)
-                    Console.Write(" ");
-
-                // Desenhar os x da linha
-                for (int x = 0; x < quantidadeDeX; x++)
-                    Console.Write("x");
-
-                quantidadeDeX -= 2;
-                quantidadeDeEspacos++;
-
-                Console.WriteLine();
-            }
-        }
+        return tamanhoDiamante;
     }
 }
