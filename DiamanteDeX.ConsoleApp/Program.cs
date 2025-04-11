9caeede249da163eef027e469c473adeca019acf
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-11 12:49:05 -0300

    Implementado desenho da parte de baixo do diamante (definitivo)

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -44,7 +44,25 @@
 
             Console.WriteLine();
 
+            quantidadeDeX -= 2;
+            quantidadeDeEspacos = 1;
+
             // Parte inferior do diamante
+            for (int linha = 0; linha < quantidadeLinhas; linha++)
+            {
+                // Desenhar os espeços da linha
+                for (int espaco = 0; espaco < quantidadeDeEspacos; espaco++)
+                    Console.Write(" ");
+
+                // Desenhar os x da linha
+                for (int x = 0; x < quantidadeDeX; x++)
+                    Console.Write("x");
+
+                quantidadeDeX -= 2;
+                quantidadeDeEspacos++;
+
+                Console.WriteLine();
+            }
 
             Console.ReadLine();
         }
